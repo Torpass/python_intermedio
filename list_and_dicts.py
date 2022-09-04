2b9bd894312438b267ff5d7c5f002b49fa951804
Torpass <pastor.jimenez.0806@gmail.com>
2022-09-04 15:30:05 -0400

    Especificaciones
    Se agregan las especificaciones del diclo for aninado para recorrer una lista con diccionarios, además de la funcionalidad del método "Items"

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -8,6 +8,7 @@ def run():
         {"first_name": "Abraham", "last_name": "Hernández"},
         {"first_name": "Luis", "last_name": "Herice"},
     ]
+
     super_dict={
         "1" : [1,2,3,4],
         "2" : [5,6,7,8],
@@ -15,12 +16,14 @@ def run():
         "4" : [13,14,15,16],
     }
 
+# tenemos un ciclo for con la variable values que agarra todos los diccionarios de la lista, posteriormente un ciclo for aninado recorre los diccionarios de values con sus 2 variables "key" y "value". El .items es un método que nos permite tranformar el diciconario para que se pueda recorrer
+ 
     for values in super_list:
         for key, value in values.items():
             print(f'{key} - {value}')
 
     for key, value in super_dict.items():
-        print(f"La key del diccionario: {key}\n El value del diccionario: {value}")
+        print(f"La key del diccionario {key}\n contiene estos valores: {value}")
 
 if __name__== "__main__":
     run()
